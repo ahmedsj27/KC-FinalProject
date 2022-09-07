cbd5154b66c459963d28d8695930354854a96d27
ahmed salem <ahmedsj2007@gmail.com>
2022-09-07 20:04:26 +0300

    ahmed salem final_project
    ahmed salem final_project

---
+++ ahmed salem final_project/Assets/scripts/HighscoreText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighscoreText : MonoBehaviour {
+
+	Text highscore;
+		void OnEnable(){
+
+		highscore = GetComponent<Text> ();
+		highscore.text =  PlayerPrefs.GetInt ("highscore") + " : ﺔﻄﻘﻧ ﻰﻠﻋﺍ".ToString();
+			
+		}	
+}
